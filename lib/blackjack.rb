990c545bfd1e25abb187baf294c667f2e4694337
Peter Kreppein <pmkreppein@gmail.com>
2018-09-17 20:54:57 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   return rand(1..11)
 end
 
-def display_card_total(x)
-  return "Your cards add up to #{x}"
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
